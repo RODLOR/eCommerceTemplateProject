36980bbd70e966aee34ebbfbde9dcf98a00e51f9
RODLOR <jamrod21@hotmail.com>
2023-01-04 20:02:39 -0400

    NavFixedAgain04/01/23-2

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -13,65 +13,64 @@ const NavBar = () => {
   // };
   return (
     <nav className="bg-gray-100 flex flex-col w-full px-4 mx-auto">
-        <div className="flex justify-between">
-          <div className="flex space-x-4">
-            <div id="logoContainer">
-              <NavLink
-                to="/"
-                className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
-              >
-                <img className="h-6 w-6 mr-1" src={logo} alt="" />
-                <span className="font-bold">Tech Commerce</span>
-              </NavLink>
-            </div>
-            <div className="hidden md:flex items-center space-x-1">
-              <NavLink
-                to="/smartphones"
-                className="py-5 px-3 text-gray-700 hover:text-gray-900"
-              >
-                Smartphones
-              </NavLink>
-              <NavLink
-                to="/tablets"
-                className="py-5 px-3 text-gray-700 hover:text-gray-900"
-              >
-                Tablets
-              </NavLink>
-              <NavLink
-                to="/laptops"
-                className="py-5 px-3 text-gray-700 hover:text-gray-900"
-              >
-                Laptops
-              </NavLink>
-              <NavLink
-                to="/videogames"
-                className="py-5 px-3 text-gray-700 hover:text-gray-900"
-              >
-                Video Games
-              </NavLink>
-              <NavLink
-                to="/deals"
-                className="py-5 px-3 text-gray-700 hover:text-gray-900"
-              >
-                Deals
-              </NavLink>
-              <NavLink
-                to="/more"
-                className={"py-5 px-3 text-gray-700 hover:text-gray-900"}
-              >
-                More...
-              </NavLink>
-            </div>
+      <div className="flex justify-between">
+        <div className="flex space-x-4">
+          <div id="logoContainer">
+            <NavLink
+              to="/"
+              className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
+            >
+              <img className="h-6 w-6 mr-1" src={logo} alt="" />
+              <span className="font-bold">Template</span>
+            </NavLink>
           </div>
-          <NavLink
-            to="/login"
-            className="self-center py-2 px-3 bg-red-400 hover:bg-red-300 text-red-900 hover:text-red-800 rounded transition duration-300"
-          >
-            Login
-          </NavLink>
+          <div className="hidden md:flex items-center space-x-1">
+            <NavLink
+              to="/smartphones"
+              className="py-5 px-3 text-gray-700 hover:text-gray-900"
+            >
+              Smartphones
+            </NavLink>
+            <NavLink
+              to="/tablets"
+              className="py-5 px-3 text-gray-700 hover:text-gray-900"
+            >
+              Tablets
+            </NavLink>
+            <NavLink
+              to="/laptops"
+              className="py-5 px-3 text-gray-700 hover:text-gray-900"
+            >
+              Laptops
+            </NavLink>
+            <NavLink
+              to="/videogames"
+              className="py-5 px-3 text-gray-700 hover:text-gray-900"
+            >
+              Video Games
+            </NavLink>
+            <NavLink
+              to="/deals"
+              className="py-5 px-3 text-gray-700 hover:text-gray-900"
+            >
+              Deals
+            </NavLink>
+            <NavLink
+              to="/more"
+              className={"py-5 px-3 text-gray-700 hover:text-gray-900"}
+            >
+              More...
+            </NavLink>
+          </div>
         </div>
-
-          <SearchField></SearchField>
+        <NavLink
+          to="/login"
+          className="self-center py-2 px-3 bg-red-400 hover:bg-red-300 text-red-900 hover:text-red-800 rounded transition duration-300"
+        >
+          Login
+        </NavLink>
+      </div>
+      <SearchField/>
     </nav>
   );
 };
